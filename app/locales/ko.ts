e9155fc4cc3f376977ca99f6879088b9a112b575
sad-up <52643147+sad-up@users.noreply.github.com>
2026-05-15 16:58:54 +0800

    Update ko.ts

--- app/locales/ko.ts
+++ app/locales/ko.ts
@@ -81,6 +81,7 @@ const ko: PartialLocaleType = {
       Clear: "채팅 지우기",
       Settings: "채팅 설정",
       UploadImage: "이미지 업로드",
+      UploadFile: "파일 업로드",
     },
     Rename: "채팅 이름 변경",
     Typing: "입력 중…",
